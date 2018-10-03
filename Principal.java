6ea93b02a923044d94e33686e27681561e1de957
felipe.valim@totvs.com.br <felipecgvalim@gmail.com>
2018-10-03 19:45:06 -0300

    Alteração 2 para o github

--- Principal.java
+++ Principal.java
@@ -3,6 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Project 42");
+		System.err.println("feuchiha");
 		System.out.println("Primeira edição do git");
 		System.out.println("Seguds edição do git");
 		System.out.println("Terceira edição do git");
